fccef5be677062d4630180fad8a51c5a86b20c9e
vikashkumar6207 <vktd828403@gmail.com>
2024-04-15 17:33:21 +0530

    Digital Clock css property

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import './Component/Component';
 // import Component from './Component/Component';
-// import DigitalClock from './DigitalClock/DigitalClock';
+import DigitalClock from './DigitalClock/DigitalClock';
 // import CounterClassComponent from './CounterClassComponent/CounterClassComponent';
-import PokemonApiClassComponent from './CounterClassComponent/2.PokemonApiClassComponent/PokemonApiClassComponent';
-import PokemonKingdomMainPage from './PokemonKingdom/Page/PokemonKingdomMainPage';
+// import PokemonApiClassComponent from './CounterClassComponent/2.PokemonApiClassComponent/PokemonApiClassComponent';
+// import PokemonKingdomMainPage from './PokemonKingdom/Page/PokemonKingdomMainPage';
 function App() {
   return (
    <>
     {/* <Component /> */}
-    {/* <DigitalClock /> */}
+    <DigitalClock />
     {/* <CounterClassComponent /> */}
     {/* <PokemonApiClassComponent /> */}
    
-    <PokemonKingdomMainPage />
+    {/* <PokemonKingdomMainPage /> */}
    
    </>
   );
