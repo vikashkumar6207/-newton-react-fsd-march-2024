6f2f4516c185c3cda3b5c4d26bb440f74dad508c
vikashkumar6207 <vktd828403@gmail.com>
2024-04-15 17:36:22 +0530

    Pokemon

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import './Component/Component';
 // import Component from './Component/Component';
-import DigitalClock from './DigitalClock/DigitalClock';
+// import DigitalClock from './DigitalClock/DigitalClock';
 // import CounterClassComponent from './CounterClassComponent/CounterClassComponent';
-// import PokemonApiClassComponent from './CounterClassComponent/2.PokemonApiClassComponent/PokemonApiClassComponent';
-// import PokemonKingdomMainPage from './PokemonKingdom/Page/PokemonKingdomMainPage';
+import PokemonApiClassComponent from './CounterClassComponent/2.PokemonApiClassComponent/PokemonApiClassComponent';
+import PokemonKingdomMainPage from './PokemonKingdom/Page/PokemonKingdomMainPage';
 function App() {
   return (
    <>
